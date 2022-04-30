4693c215d3fdacf0079334c3d89e7e96ef2dbef4
maxwell arrigona <maxwellarrigona@gmail.com>
2022-04-30 08:22:23 -0600

    Update form.js

--- form.js
+++ form.js
@@ -114,31 +114,3 @@ el.style.display = 'none';
     }
   }
 })();
-
-
-
-   
-$recaptcha = $_POST['g-recaptcha-response'];
-$res = reCaptcha($recaptcha);
-if($res['success']){
-  // Send email
-}else{
-  // Error
-}
-
-function reCaptcha($recaptcha){
-  $secret = "6Lc_lKMfAAAAAMbDbkLSIFqdFG18DImlj0zwp2ho";
-  $ip = $_SERVER['REMOTE_ADDR'];
-
-  $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
-  $url = "https://www.google.com/recaptcha/api/siteverify";
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-  $data = curl_exec($ch);
-  curl_close($ch);
-
-  return json_decode($data, true);
-}
