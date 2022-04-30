2c5dae51b5dbb294c565096084c89fe8a8f6eb55
maxwell arrigona <maxwellarrigona@gmail.com>
2022-04-30 08:36:50 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,4 +1,31 @@
 
+$recaptcha = $_POST['g-recaptcha-response'];
+$res = reCaptcha($recaptcha);
+if($res['success']){
+  // Send email
+}else{
+  // Error
+}
+
+function reCaptcha($recaptcha){
+  $secret = "6LfKDV8fAAAAAFVHi0ZTWUGFc94aQwXD5lsys1ZL";
+  $ip = $_SERVER['REMOTE_ADDR'];
+
+  $postvars = array("secret"=>$secret, "response"=>$recaptcha, "remoteip"=>$ip);
+  $url = "https://www.google.com/recaptcha/api/siteverify";
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+  $data = curl_exec($ch);
+  curl_close($ch);
+
+  return json_decode($data, true);
+}
+
+
+
 // supposedly assigns a token when form is submitted
    function onSubmit(token) {
      document.getElementById("form").submit();
